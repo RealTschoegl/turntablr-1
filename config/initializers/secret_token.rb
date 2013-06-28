93388bbe77e4fecb581fe5bf49f64026a3bfdcef
MakerSquare Student <student@lab-5.att.net>
2013-06-28 09:54:29 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Turntablr::Application.config.secret_token = '904c5f7702e430537db084f89d4cca067b83c38ff497ecdb4f4eba970149774333c9c5fa7de6458aeb2e043b0cb658b199928e72f487cd5001aa6781aa3af692'
